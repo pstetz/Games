54c789458c7d4644c9c7a96fd4ec768d0b8bd310
pstetz <pstetz@berkeley.edu>
2018-07-25 23:52:40 -0700

    keeps track of turns

--- in_progress/Go/js/main.js
+++ in_progress/Go/js/main.js
@@ -1,11 +1,22 @@
 console.log("loaded main.js");
 
+let players = {0: "White", 1: "Black"}
+
+let curr_turn = 0
+
 $( document ).ready(function() {
 
 	$( "#board" ).mousemove(function(event) {
-		let mouse_tracker = $("#mouse_tracker")
-		let tracker = "<div>X: " + event.clientX + "  Y: " + event.clientY + "</div>"
-		mouse_tracker[0].innerHTML = tracker;
+		let mouse_tracker = $("#mouse_tracker");
+		let text = "X: " + event.clientX + "  Y: " + event.clientY
+		mouse_tracker[0].innerHTML = text;
+	});
+
+	$( "#board" ).click(function(event) {
+		curr_turn = 1 - curr_turn;
+		let turn_tracker = $("#turn_tracker");
+		let text = players[curr_turn] + "'s' turn";
+		turn_tracker[0].innerHTML = text;
 	});
 
 });
