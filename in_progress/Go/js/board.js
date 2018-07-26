a6917e19e6671b9a4fbcaf6bae946fa35d3c2853
pstetz <pstetz@berkeley.edu>
2018-07-26 00:17:45 -0700

    initialize grid method

--- in_progress/Go/js/board.js
+++ in_progress/Go/js/board.js
@@ -4,10 +4,12 @@ let players = {0: "White", 1: "Black"}
 
 class Board {
 	constructor() {
+		this.row = 19;
+		this.col = 19;
 		this.size = 0;
 		this.curr = 0;
 		this.player = "White";
-		console.log("made board");
+		this.init_grid();
 	}
 
 	place_stone(move) {
@@ -18,11 +20,22 @@ class Board {
 	change_turns() {
 		this.curr = 1 - this.curr;
 		this.player = players[this.curr];
-		console.log(this.curr);
 	}
 
 	is_good_move(event) {
 		return true;
+	}
+
+	init_grid() {
+		let grid = new Array(this.row);
+		for(let i = 0; i < this.row; i++) {
+			let row = new Array(this.col);
+			for(let j = 0; j < this.col; j++) {
+				row[j] = "Empty";
+			}
+			grid[i] = row;
+		}
+		self.grid = grid;
 	}
 
 }
