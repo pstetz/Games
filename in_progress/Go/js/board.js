1cdbfc8e480edc0d540f3578f8dfe2eae164afa6
pstetz <pstetz@berkeley.edu>
2018-07-26 00:09:36 -0700

    created board class

--- in_progress/Go/js/board.js
+++ in_progress/Go/js/board.js
@@ -1,3 +1,28 @@
 console.log("loaded board.js");
 
-let board = $("#board");
+let players = {0: "White", 1: "Black"}
+
+class Board {
+	constructor() {
+		this.size = 0;
+		this.curr = 0;
+		this.player = "White";
+		console.log("made board");
+	}
+
+	place_stone(move) {
+		this.change_turns();
+		this.size += 1;
+	}
+
+	change_turns() {
+		this.curr = 1 - this.curr;
+		this.player = players[this.curr];
+		console.log(this.curr);
+	}
+
+	is_good_move(event) {
+		return true;
+	}
+
+}
--- in_progress/Go/js/main.js
+++ in_progress/Go/js/main.js
@@ -1,8 +1,6 @@
 console.log("loaded main.js");
 
-let players = {0: "White", 1: "Black"}
-
-let curr_turn = 0
+let board = new Board();
 
 $( document ).ready(function() {
 
@@ -13,10 +11,12 @@ $( document ).ready(function() {
 	});
 
 	$( "#board" ).click(function(event) {
-		curr_turn = 1 - curr_turn;
-		let turn_tracker = $("#turn_tracker");
-		let text = players[curr_turn] + "'s' turn";
-		turn_tracker[0].innerHTML = text;
+		if(board.is_good_move(event)) {
+			board.place_stone(event);
+			$("#turn_tracker")[0].innerHTML = board.player + "'s turn";
+		} else {
+			alert("Try a different move");
+		}
 	});
 
 });
